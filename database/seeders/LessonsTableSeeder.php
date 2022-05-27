8bad102f37aa4bccc83029b2c166230e8429ee0c
Manuel Strobel <strobel.manuel@yahoo.de>
2022-05-27 20:29:28 +0200

    Update kurs klappt nicht, zeit wird automatisch befüllt...

--- database/seeders/LessonsTableSeeder.php
+++ database/seeders/LessonsTableSeeder.php
@@ -27,30 +27,30 @@ class LessonsTableSeeder extends Seeder
         $lesson1 = new Lesson();
         $lesson1->title = "Coole Übung";
         $lesson1->description = "Wir frischen Komm. auf!";
-        $lesson1->timeslot1 = date("2022-5-21 13:00:00");
+        $lesson1->timeslot1 = new \DateTime("2022-05-22UTC15:00");
         $lesson1->user()->associate($user1);
 
         $lesson2 = new Lesson();
         $lesson2->title = "Lernen macht Spaß";
         $lesson2->description = "APA leicht erklärt";
-        $lesson2->timeslot1 = date("2022-5-22 18:00:00");
-        $lesson2->timeslot2 = date("2022-5-22 20:30:00");
+        $lesson2->timeslot1 = new \DateTime("2022-05-22UTC12:00");
+        $lesson2->timeslot2 = new \DateTime("2022-05-22UTC11:00");
         $lesson2->user()->associate($user1);
 
         $lesson3 = new Lesson();
         $lesson3->title = "Heute werden wir schlauer";
         $lesson3->description = "Wir lernen zusammen etwas über APA";
         $lesson3->taker = 2;
-        $lesson3->timeslot1 = date("2022-5-22 16:30:00");
-        $lesson3->timeslot2 = date("2022-5-22 11:15:00");
+        $lesson3->timeslot1 = new \DateTime("2022-05-22UTC15:00");
+        $lesson3->timeslot2 = new \DateTime("2022-05-22UTC18:00");
         $lesson3->user()->associate($user1);
 
         $lesson4 = new Lesson();
         $lesson4->title = "Vierte Lesson";
         $lesson4->description = "ABCDEFG";
         $lesson4->taker = 2;
-        $lesson4->timeslot1 = date("2022-5-22 16:30:00");
-        $lesson4->timeslot2 = date("2022-5-22 11:15:00");
+        $lesson4->timeslot1 = new \DateTime("2022-05-22UTC15:00");
+        $lesson4->timeslot2 = new \DateTime("2022-05-22UTC17:00");
         $lesson4->user()->associate($user1);
 
         //Speichere Einheiten ab und weiße diese den Kursen zu
